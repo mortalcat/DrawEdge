5d1de678f7b92b9a6abdca8589dfd9a1c3d475fb
mortalcat <mortalcat@hotmail.com>
2020-03-11 19:12:38 +0800

    Add files via upload

--- poly.py
+++ poly.py
@@ -7,8 +7,10 @@
 
 ####pre-processing - denoising
 import cv2
-import EdgeDraw
-def saliencySampling(img):
+from EdgeDraw import EdgeDraw
+import math
+import numpy as np
+def saliency_sampling(img):
     '''
 
     :param img:
@@ -20,7 +22,7 @@ def saliencySampling(img):
                               cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]#get binary map
     #TODO,random sampling N,default N = Floor(Lw/Li) × Floor(Lh/Li)
 
-def getEdgeFeatures(im):
+def get_edge_features(im):
     '''
     edge figures
     :return:
@@ -29,10 +31,50 @@ def getEdgeFeatures(im):
 
 
 
-def getDistanceMap(im, edges):
+def get_distance_map(im, edges):
     #form distance map
     #https://github.com/mirrorworld/VoronoiJumpFlood/blob/master/jump.py
     pass
+
+
+def jump_flooding(edges, shape):
+    #round up, 7 =>3
+    distanceMap, featureMap = np.nan*np.empty(shape), np.nan*np.empty(shape)
+    steps = math.ceil(math.log(max(shape)))
+    #initiation
+    for idx,c in enumerate(edges):
+        x, y = c
+        distanceMap[x,y] = 0
+        featureMap[x,y] = idx
+    for idxS in range(steps):
+        window = pow(2, steps-idxS-1)
+        jump_once(distanceMap, featureMap, window, shape)
+    return distanceMap, featureMap
+
+
+        #check 9 points
+def jump_once(distanceMap, featureMap,  window, shape):
+    for idxR in range(0, shape[0],window):
+        for idxC in range(0, shape[1],window):
+            if featureMap[idxR, idxC] == np.nan:#this is not related to any seed yet
+                continue
+            for idxRf in range(idxR-window, idxR, idxR+window):
+                for idxCf in range(idxC-window, idxC, idxC+window):
+                    d = distance((featureMap[idxR, idxC], (idxRf, idxCf)))
+                    if d < distanceMap[idxRf, idxCf]:
+                        distanceMap[idxRf, idxCf] = d
+                        featureMap[idxRf, idxCf] = featureMap[idxR, idxC]#update nearest edge
+
+
+
+def distance(p1, p2):
+    dx = p1[0] - p2[0]
+    dy = p1[1]-p2[1]
+    return math.sqrt(dx*dx+dy*dy)
+
+
+
+
 
 
 
