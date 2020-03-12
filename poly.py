2987308555dbe3b7b805b8e686826cb3d3c47cee
mortalcat <mortalcat@hotmail.com>
2020-03-12 18:31:00 +0800

    Add files via upload

--- poly.py
+++ poly.py
@@ -10,6 +10,13 @@ import cv2
 from EdgeDraw import EdgeDraw
 import math
 import numpy as np
+
+def distance(p1, p2):
+    dx = p1[0] -p2[0]
+    dy = p1[1]-p2[1]
+    return math.sqrt(dx*dx+dy*dy)
+
+
 def saliency_sampling(img):
     '''
 
@@ -22,19 +29,48 @@ def saliency_sampling(img):
                               cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]#get binary map
     #TODO,random sampling N,default N = Floor(Lw/Li) × Floor(Lh/Li)
 
-def get_edge_features(im):
+def get_edges(im,window =4):
     '''
     edge figures
     :return:
     '''
-    return EdgeDraw(im).run()
+    edges = EdgeDraw().run(im,window)
+    #edges = [np.array(e).reshape((len(e),1,2)) for e in edges]
+    edges = np.array(edges).reshape(len(edges), 1,2)
+
+    return edges
+
+def mid_point(p1,p2):
+    return (math.ceil((p1[0]+p2[0])/2),math.ceil((p1[1]+p2[1])/2))
+
+
+def approximate_contour(raw, distance_constraint = 8.2, e_factor =0.00001 ):
+    epsilon = e_factor * cv2.arcLength(raw, True)
+    raw_approx = cv2.approxPolyDP(raw, epsilon, False)
+  #  approx = []
+  #  for idxP in range(len(raw_approx)-1):
+  #      left = raw_approx[idxP][0]
+  #      right = raw_approx[idxP + 1][0]
+  #      if distance(left, right) > distance_constraint:
+  #          mid = mid_point(left, right)
+  #          approx.extend([left,mid])
+  #      else:
+  #          approx.append(left)
+  #  approx.append(raw_approx[len(raw_approx)-1][0])
+    return raw_approx
 
 
 
-def get_distance_map(im, edges):
+
+
+def edge_sampling(cnt):
+    pass
+
+
+def get_distance_map(d_map, m):
     #form distance map
     #https://github.com/mirrorworld/VoronoiJumpFlood/blob/master/jump.py
-    pass
+    return np.where(d_map/m%2==0, 255/m*(1-d_map%m),255/m*d_map%m)
 
 
 def jump_flooding(edges, shape):
@@ -67,16 +103,38 @@ def jump_once(distanceMap, featureMap,  window, shape):
 
 
 
-def distance(p1, p2):
-    dx = p1[0] - p2[0]
-    dy = p1[1]-p2[1]
-    return math.sqrt(dx*dx+dy*dy)
 
 
+##Utility function
+def showImage( name, img):
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+##Utility function
+def edges2Image(plist, shape):
+    pointFig = np.zeros(shape, np.uint8)
+    for pointX, pointY in plist:
+        pointFig[pointX, pointY] = [255, 255, 255]
+
+    return pointFig
 
 
-
+def helper(addr):
+    im = cv2.imread(addr)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, 1, 2)
+    rawCnt = get_edges(im,2)
+    #rawCnt = [(50,50),(50,100),(100,50),(100,100),(44,111),(51,11),(13,43)]
+    #rawCnt = np.array(rawCnt).reshape((len(rawCnt),1,2))
+    approCnt,raw_a = approximate_contour(rawCnt)
+    showImage('contours', edges2Image(rawCnt.reshape(len(rawCnt),2), im.shape))
+    showImage('contours', edges2Image(raw_a.reshape(len(raw_a),2), im.shape))
+    showImage('contours', edges2Image(approCnt, im.shape))
 
 
 
 ####edge tracing sampling on edge
+if __name__ =='__main__':
+    helper('images/mouse.jpg')
